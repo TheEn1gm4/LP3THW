18d0a3153755480f6e79c070882e0951493f4c61
TheEn1gm4 <130586601+TheEn1gm4@users.noreply.github.com>
2023-04-12 10:33:13 -0600

    Add files via upload

---
+++ ex3.py
@@ -0,0 +1,17 @@
+my_name ="Andres Hernández"
+my_age= 22 #no es coto
+my_height= 176 #mts
+my_weight=70 #kg
+my_eyes= 'cafe'
+my_teeth='blancos'
+my_hair='cafe'
+
+print(f"Hablemos de {my_name}. ")
+print(f"El mide {my_height} mts. ")
+print(f"El pesa {my_weight} kg. ")  
+print (f"El de hecho no es tan gordito. ") 
+print(f"El tiene ojos {my_eyes} y su cabello es {my_hair}. ")
+
+#ESta linea esta truqueada 
+total=my_age + my_height + my_weight
+print(f"si yo sumo {my_age}, {my_height} y mi {my_weight} obtengo el {total}")
