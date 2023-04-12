43c6667ce8277eea70fd45068422ef4b172ccb53
TheEn1gm4 <130586601+TheEn1gm4@users.noreply.github.com>
2023-04-12 11:25:02 -0600

    Add files via upload

---
+++ ex9.py
@@ -0,0 +1,8 @@
+print("Cuantos años tienes? ", end= ' ')
+age=input()
+print("Cuanto mides? ", end=' ')
+height=input()
+print("Cuanto pesas? ", end=' ')
+weight=input()
+
+print(f"Asi que, tu tienes {age} , mides {height} y pesas {weight}.")
