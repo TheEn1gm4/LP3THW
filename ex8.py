6b3f9ed02149752d342c5fd8f4821a7ca6ec3eb7
TheEn1gm4 <130586601+TheEn1gm4@users.noreply.github.com>
2023-04-12 11:21:46 -0600

    Add files via upload

---
+++ ex8.py
@@ -0,0 +1,14 @@
+tabby_cat= "\tIm tabbed in. "
+persian_cat= "Im split\non a line. "
+backlash_cat= "Im \\ a \\ cat. "
+
+fat_cat= """
+I'll do a list:
+\t* cat food
+\t* Fishies
+\t* CAtnip\n\t* Grass
+"""
+print(tabby_cat)
+print(persian_cat)
+print(backlash_cat)
+print(fat_cat)
