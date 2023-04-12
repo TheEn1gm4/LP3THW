3ad462d9ff910bcf15eb6e36d58dafdf1f3801a3
TheEn1gm4 <130586601+TheEn1gm4@users.noreply.github.com>
2023-04-12 10:10:01 -0600

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,10 @@
+print("Hello World!")
+print("Hola de novo")
+print("Disfruto escribir esto." )
+print("ESto es divertido. ")
+print('WOOH, imprimiendo')
+print("Preferiría que 'no'. ")
+print('Yo "digo" que no te metas.' )
+
+
+
