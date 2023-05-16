98b45a8ce38d885f5aeb88a461610f1263a05594
TheEn1gm4 <130586601+TheEn1gm4@users.noreply.github.com>
2023-05-15 18:46:08 -0600

    Add files via upload

---
+++ ex12.py
@@ -0,0 +1,9 @@
+from sys import argv
+#reaad the wyss seection for how to run this
+script, first, second, third= argv
+
+print("the sccrip is called ", script)
+print("la primer variable es ", first)
+print("la segunda variable es ", second)
+print("la tercer variable es ", third)
+
